b0f5bb86793ead659a0d5873d1aa3da3993b4023
Aine <anniermfc@gmail.con>
2018-02-27 19:48:53 -0700

    checkpoint 4: pt3

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -5,8 +5,6 @@ class MessageList extends Component{
   constructor(props) {
     super(props);
 
-    /*this.assignMessage = this.assignMessage.bind(this);*/
-
     this.state = {
       messages: [],
       messagesToShow: {username: "", content: "", sentAt: "", roomID: ""}
@@ -20,12 +18,7 @@ class MessageList extends Component{
   componentDidMount() {
       console.log(this.props.activeRoomID);
       console.log(this.messagesRef.child('Messages'));
-      this.messagesRef.child('messages')
-      /*
-          .orderByChild(this.props.activeRoomID)
-          .equalTo(this.props.activeRoomID)
-      */
-          .on('child_added', snapshot => {
+      this.messagesRef.child('messages').on('child_added', snapshot => {
         const message = snapshot.val();
         message.key = snapshot.key;
         console.log(snapshot);
@@ -33,70 +26,9 @@ class MessageList extends Component{
       });
   }
 
-/*  assignMessage(e) {
-    e.preventDefault();
-    if(this.newMessage.value === '') {return}
-
-    var roomInfo = {
-        username: '',
-        content: this.newMessage.value,
-        sentAt: this.props.firebase.database.ServerValue.TIMESTAMP,
-        roomId: this.props.activeRoom
-      }
-
-    this.messagesRef.push({
-      messages: roomInfo,
-    });
-
-
-    this.newMessage.value = '';
-    this.setState({messagesToShow: []});
-
-    /*this.messagesFilter(roomInfo.roomId);*/
-/*    console.log(this.state.messages)
-  }
-
-  messagefilter(roomId) {
-    var info = [];
-    this.messagesRef.on('value', function(snapshot) {
-      var data = snapshot.val();
-      Object.entries(data).forEach(
-        ([key,value]) => info.push(value)
-      );
-    });
-    var key = roomId;
-
-    for(let i =0; i < info.length; i++) {
-      if(info[i].message.roomId === key) {
-        this.fill(info[i].message.content)
-      }
-    }
-  }
-
-  fill(x) {
-    this.setState({ messagesToShow: this.state.messagesToShow.push(x)})
-  }*/
 
   render() {
-    const isRoomChosen = this.props.activeRoom !== '';
     return (
-      /*
-    <div>
-      <div className="message-list">
-      {isRoomChosen ? (
-          <form onSubmit={this.assignMessage}>
-          <input type="text" ref={(value) => this.newMessage = value}/>
-          <input type="submit" value="Post Message"/>
-          </form>
-      ) : (null)}
-      </div>
-
-
-      <div className="message-info">
-      <ul>
-       {isRoomChosen ? (
-          this.state.messages.map( (message,index) => {
-            return  <li className="message" key={index}>{message.name}</li>*/
 
             <div className="message-list">
               <div>
@@ -113,28 +45,10 @@ class MessageList extends Component{
               }
               </div>
 
-            );
-          }
-
-        }
-/*s
-                     <div className="username">{message.username}</div>
-                    <div>
-                    <span className="message-content">{message.content}</span>
-                    <span className="time-sent">{message.sentAt}</span>
-                    </div>
-                  </div>
-                     if(message.roomId === this.props.activeRoomId)
-            })
-
+    );
+  }
 
 }
-
-
-
-          ) : (null)}
-        </ul>
-      </div>*/
 
 
 
